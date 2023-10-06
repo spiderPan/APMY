f386386b687b4f972acc442f6c9bb207aff32b7b
spiderPan <panbanglanfeng@gmail.com>
2023-10-06 12:16:48 -0400

    download as image from the post

---
+++ webpack/main.js
@@ -0,0 +1,21 @@
+import domtoimage from 'dom-to-image';
+
+let download_btn = document.getElementById('download-btn');
+
+download_btn.onclick = function () {
+    domtoimage.toJpeg(
+        document.getElementById('post-content'),
+        {
+            quality: 0.95,
+            bgcolor: '#fff',
+            style: { padding: '15px 25px 15px 25px' }
+        })
+        .then(function (dataUrl) {
+            var link = document.createElement('a');
+            link.download = 'my-image-name.jpeg';
+            link.href = dataUrl;
+            link.click();
+        });
+}
+
+
