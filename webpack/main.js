7ed7153a6ad16bf245153696ac9820e6db6e48ec
spiderPan <panbanglanfeng@gmail.com>
2023-10-06 12:49:59 -0400

    build the webpack inside docker and fix the padding issue

--- webpack/main.js
+++ webpack/main.js
@@ -4,18 +4,28 @@ let download_btn = document.getElementById('download-btn');
 
 if (download_btn) {
     download_btn.onclick = function () {
+        let post_content = document.getElementById('post-content');
+
+        if (!post_content) {
+            return;
+        }
+
+        post_content.style.padding = '15px 25px 15px 25px';
+
         domtoimage.toJpeg(
-            document.getElementById('post-content'),
+            post_content,
             {
                 quality: 0.95,
-                bgcolor: '#fff',
-                style: { padding: '15px 25px 15px 25px' }
+                bgcolor: '#fff'
             })
             .then(function (dataUrl) {
                 var link = document.createElement('a');
                 link.download = 'my-image-name.jpeg';
                 link.href = dataUrl;
                 link.click();
+            }).catch(function (error) {
+                // Handle any errors that occur during the conversion
+                console.error('Error converting DOM to image:', error);
             });
     }
 }
